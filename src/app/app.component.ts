1f65635b6d8830e7382a4ea8208f6ba4d00998bf
Avela Pato <avela.pato@liberty.co.za>
2020-09-13 12:56:52 +0200

    Latest changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./index.html",
 })
 export class AppComponent {
-  title = 'mm-vip-security';
+  title = "mm-vip-security";
 }
